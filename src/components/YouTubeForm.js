d3674b489ef68d76e9ceb0bf3f9a64e531d8e602
Rahul Varal <76585698+rahulvaral@users.noreply.github.com>
2022-07-25 18:37:50 +0530

    added form

---
+++ src/components/YouTubeForm.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { useFormik } from "formik";
+
+function YouTubeForm() {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      channel: ""
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    }
+  });
+
+  return (
+    <div className="form">
+      <form onSubmit={formik.handleSubmit}>
+        <label htmlFor="name"> Name </label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          onChange={formik.handleChange}
+          value={formik.values.name}
+        />
+        <label htmlFor="email"> Email </label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
+        <label htmlFor="channel"> Channel </label>
+        <input
+          type="text"
+          id="channel"
+          name="channel"
+          onChange={formik.handleChange}
+          value={formik.values.channel}
+        />
+        <button type="submit"> Submit </button>
+      </form>
+    </div>
+  );
+}
+
+export default YouTubeForm;
