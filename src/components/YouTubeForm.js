f62c3746ff700ce81efb08c2867533c0b736461f
Rahul Varal <76585698+rahulvaral@users.noreply.github.com>
2022-07-25 19:19:31 +0530

    Added Validation

--- src/components/YouTubeForm.js
+++ src/components/YouTubeForm.js
@@ -10,6 +10,19 @@ function YouTubeForm() {
     },
     onSubmit: (values) => {
       console.log(values);
+    },
+    validate: (values) => {
+      let errors = {};
+      if (!values.name) {
+        errors.name = " Name is required";
+      }
+      if (!values.email) {
+        errors.email = " Email is required";
+      }
+      if (!values.channel) {
+        errors.channel = " Channel is required";
+      }
+      return errors;
     }
   });
 
@@ -22,24 +35,38 @@ function YouTubeForm() {
           id="name"
           name="name"
           onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
           value={formik.values.name}
         />
+        <div className="form-error">
+          {formik.errors.name && formik.touched.name && formik.errors.name}
+        </div>
         <label htmlFor="email"> Email </label>
         <input
           type="email"
           id="email"
           name="email"
           onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
           value={formik.values.email}
         />
+        <div className="form-error">
+          {formik.errors.email && formik.touched.email && formik.errors.email}
+        </div>
         <label htmlFor="channel"> Channel </label>
         <input
           type="text"
           id="channel"
           name="channel"
           onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
           value={formik.values.channel}
         />
+        <div className="form-error">
+          {formik.errors.channel &&
+            formik.touched.channel &&
+            formik.errors.channel}
+        </div>
         <button type="submit"> Submit </button>
       </form>
     </div>
